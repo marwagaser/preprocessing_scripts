7135f327e4a733aa80fcb51280070d0a0f29b8c0
Marwa <marwagaser@gmail.com>
2021-10-07 13:41:02 +0200

    feat: added arabic and english preprocessing scripts

---
+++ arabic_normalization.py
@@ -0,0 +1,26 @@
+
+from camel_tools.utils.dediac import dediac_ar
+from camel_tools.tokenizers.word import simple_word_tokenize
+
+
+from camel_tools.utils.normalize import normalize_alef_maksura_ar
+from camel_tools.utils.normalize import normalize_alef_ar
+import re
+
+tokenized = []
+def ortho_normalize(text):
+    text = normalize_alef_maksura_ar(text)
+    text = normalize_alef_ar(text)
+    return text
+def remove_diacritics(text):
+     return dediac_ar(text)
+with open ("/home/marwagaser/corpora/Round3/Round3/LDC2021T15/LDC2021T15_1.eg") as textfile: #enter file name here
+    for line in textfile:
+      line = simple_word_tokenize(ortho_normalize(remove_diacritics(line)))
+      line = ' '.join(line)
+      line = re.sub('\s{2,}', ' ', line) #remove extra spaces in the line
+      tokenized.append(line)
+with open ("/home/marwagaser/corpora/Round3/Round3/LDC2021T15/LDC2021T15_2.eg","w") as output: #file to store the results in
+        output.write("\n".join(map(str, tokenized)))
+
+        
---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import re
+import preprocessor as p
+import emoji
+p.set_options(p.OPT.URL, p.OPT.MENTION,p.OPT.HASHTAG,p.OPT.RESERVED, p.OPT.SMILEY) #emoji option removed as it messes up Arabic letters
+preprocessedtext = []
+def unifyapostrophe(line):
+    return re.sub('\’','\'',line)
+def remove_emojis(text):
+    return emoji.get_emoji_regexp().sub(r'', text)
+def twitterprocessor(line):
+    line = remove_emojis(line)
+    return p.clean(line) #remove URL, hashtag, emoticons, and emojis
+def tokenizeContractions(line):
+    return re.sub(r"n't|'(?:ll|[vr]e|[msd])", r" \g<0>", line)#tokenize contractions
+def tokenizeNumerics(line):
+    return re.sub(r'(?<=[^0-9\s])(?=[0-9])|(?<=[0-9])(?=[^0-9\s])'," ",line)#tokenize numerics
+def removeDuplicates(line):#remove more than two duplicates
+    return re.sub(r"(.)\1+", r"\1\1", line) #remove >2 reptitions
+def tokenizePunc(line):
+    return re.sub(r"([^\w\s])", r' \1 ', line) #tokenize punctuation
+def chooseIntended(line):
+   return re.sub(r"\[\s*([^][|]*?)\s*\|[^][]*]", r"\1", line)
+def removeextraspace(line):
+    return re.sub('\s{2,}', ' ', line)
+def lowercase(line):
+    return line.lower()
+def remove_trailing_leading_spaces(line):
+    return line.strip()
+def removeTypoCorrectionSign(line): #remove = sign before word
+    return re.sub(r"([=])([a-zA-Z]+)",r"\2",line)
+def removeBestTranslationSign(line):
+    return re.sub(r"\(\s*\(\s*(.*?)\s*\)\s*\)",r"\1",line) #Remove (()) for best translation guess
+def remove_tags(line):
+    return re.sub(r"\<|\>|\«|\»","",line)
+with open ("/home/marwagaser/c1.txt","r") as textfile:#(change file name here)
+    for line in textfile:
+        line = lowercase(remove_trailing_leading_spaces(removeextraspace(remove_tags(removeBestTranslationSign(tokenizePunc(tokenizeContractions(tokenizeNumerics(removeDuplicates(removeTypoCorrectionSign(twitterprocessor(chooseIntended(unifyapostrophe(line)))))))))))))
+        preprocessedtext.append(line)
+with open ("/home/marwagaser/c2.txt","w") as output: #change file name to store results in here
+       corpora = "\n".join(preprocessedtext)
+       output.write(corpora)
