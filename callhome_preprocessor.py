f00d0c5ecb42f5d12e469a733d6d8b91ee6471b2
Marwa <marwagaser@gmail.com>
2021-10-19 01:31:07 +0200

    fix: fixed double hyphen, and noun annotation

--- callhome_preprocessor.py
+++ callhome_preprocessor.py
@@ -16,22 +16,21 @@ def removeLanguageTags_MSA_and_Others(line):
 def removePartialwords(line):
     return re.sub(r'(?<!\S)(-\S+-)(?!\S)|(?<!\S)-\S+(?![\S-])|(?<![\S-])\S+-(?!\S)', r'\1', line)
 def removeProperNounAnnotation(line):
-    return re.sub(r'(?<!\S)&(\S+)(?![\S&])', r"\1", line)
+    return re.sub(r'(?<!\S)(&\S+&)(?!\S)|(?<!\S)&(\S+)(?![\S&])', r"\1", line)
 def removeTrailingandLeadingDoubleHyphens(line):
     # remove -- in -> -- translated sentence -- -> translated sentence
-    return re.sub(r"(\t\-\-)|(\-\-$)","\t",line)
+    return re.sub(r"(?<!\S)--(?![\S])","\t",line)
 preprocessedtext=[]
-
 with open ("/home/marwagaser/preprocessing_scripts/tgt_4","r") as textfile:#(change file name here)
     for line in textfile:
         line = line.strip()
-        line = removePartialwords(removeProperNounAnnotation(removeLanguageTags_MSA_and_Others(
+        line = removeTrailingandLeadingDoubleHyphens(removePartialwords(removeProperNounAnnotation(removeLanguageTags_MSA_and_Others(
             removeLanguageTags_European_and_English(
             removeDoubleAsterisk(
                 removeIntermittentNoise(
                     removeTalkNoise(
                 removeDoubleSquareParentheses(
-                    removeDoubleParentheses(removeTrailingandLeadingDoubleHyphens(line))
+                    removeDoubleParentheses(line))
                     )
                     )
                     )
