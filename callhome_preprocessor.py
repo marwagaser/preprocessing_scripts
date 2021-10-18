26dfcaacaf3a98280b7d8c1c7700cca8d78fd4fc
Marwa <marwagaser@gmail.com>
2021-10-19 01:45:06 +0200

    fix: fixed noun annotation regex

--- callhome_preprocessor.py
+++ callhome_preprocessor.py
@@ -16,11 +16,12 @@ def removeLanguageTags_MSA_and_Others(line):
 def removePartialwords(line):
     return re.sub(r'(?<!\S)(-\S+-)(?!\S)|(?<!\S)-\S+(?![\S-])|(?<![\S-])\S+-(?!\S)', r'\1', line)
 def removeProperNounAnnotation(line):
-    return re.sub(r'(?<!\S)(&\S+&)(?!\S)|(?<!\S)&(\S+)(?![\S&])', r"\1", line)
+    return re.sub(r'(?<!\S)&(\S+)(?![\S&])', r"\1", line)
 def removeDoubleHyphens(line):
     # remove -- in -> -- translated sentence -- -> translated sentence
     return re.sub(r"(?<!\S)--(?![\S])","\t",line)
 preprocessedtext=[]
+
 with open ("/home/marwagaser/preprocessing_scripts/tgt_4","r") as textfile:#(change file name here)
     for line in textfile:
         line = line.strip()
